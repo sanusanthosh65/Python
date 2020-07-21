5a05c9d3e48f82a4610ee6a3abf0a1d6e41adab6
sanusanthosh65 <68541342+sanusanthosh65@users.noreply.github.com>
2020-07-21 23:13:09 +0530

    Add files via upload

---
+++ Untitled2 (1).py
@@ -0,0 +1,304 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # syntax
+
+# In[154]:
+
+
+print ('hi')
+
+
+# In[143]:
+
+
+print ("hello \  python")
+
+
+# In[139]:
+
+
+print (""""hello\n python""" )
+
+
+# In[140]:
+
+
+print (""""hello\t python""" )
+
+
+# In[152]:
+
+
+name = "sanu"
+marks = 95.09
+age = 19
+print ("The  name of person is ",name,"marks is ",marks,"age is",age)
+
+
+# In[153]:
+
+
+print ("The  name of person is %10s marks is %10.2f age is %10d"%(name,marks,age))
+
+
+# # variables
+
+# In[159]:
+
+
+a=10
+b=20
+id(a)
+
+
+# In[160]:
+
+
+id(b)
+
+
+# # Operators and types.
+
+# # arithamatic operators
+
+# In[163]:
+
+
+2*4
+
+
+# In[164]:
+
+
+10/3
+
+
+# In[165]:
+
+
+10//3
+
+
+# In[166]:
+
+
+10%3
+
+
+# # comparison operator
+
+# In[167]:
+
+
+a=10
+b=2
+a==b
+
+
+# In[168]:
+
+
+a<=b
+
+
+# In[169]:
+
+
+a>=b
+
+
+# In[170]:
+
+
+a!=b
+
+
+# # assignment operatoer 
+
+# In[174]:
+
+
+a=10
+10*40
+
+
+# # bw oprators
+
+# In[178]:
+
+
+a=240
+b=1
+a | b
+
+
+# In[179]:
+
+
+bin(a)
+
+
+# In[180]:
+
+
+bin(b)
+
+
+# In[182]:
+
+
+a & b
+
+
+# In[184]:
+
+
+a ^ b
+
+
+# In[185]:
+
+
+a << b
+
+
+# In[183]:
+
+
+a | b
+
+
+# # logical op
+
+# In[186]:
+
+
+a = 1
+b = 20
+a > b
+
+
+# In[188]:
+
+
+a>20  or 10 >9
+
+
+# In[190]:
+
+
+a>20  & 10 >9
+
+
+# In[196]:
+
+
+10 > 9 and 20 > m
+
+
+# In[198]:
+
+
+not 10<9
+
+
+# In[197]:
+
+
+10 < 9 and 20 > m
+
+
+# # membership op
+
+# In[202]:
+
+
+strl= "python programming"
+"x" in strl
+
+
+# In[204]:
+
+
+strl= "python programming"
+"x" not in strl
+
+
+# In[205]:
+
+
+a=10
+b=10
+a==b
+
+
+# In[206]:
+
+
+a is b
+
+
+# In[207]:
+
+
+id(b)
+
+
+# In[212]:
+
+
+x=1.5
+y=1.5
+x == y
+
+
+# In[215]:
+
+
+x is y
+
+
+# In[213]:
+
+
+y is x
+
+
+# In[217]:
+
+
+id(x)
+
+
+# In[227]:
+
+
+a = 257
+b = 257
+a==b
+
+
+# In[229]:
+
+
+a is not b
+
+
+# In[230]:
+
+
+10/20*5555555555555555555555555545555555555555555555555505555555555555555555555555555555555555555555555555555555
+
+
+# In[232]:
+
+
+10+20/60
+
+
+# In[233]:
+
+
+555555555555555555555555555555555555555555555**-1
+
